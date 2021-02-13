5918ec13bfbef0328d7b8ea934e0dcd82bbb04ea
pool <loop.edward@gmail.com>
2021-02-13 15:23:34 +0800

    Create seeder for transactions

---
+++ database/seeders/TransactionSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use DateTime;
+
+class TransactionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('transactions')->insert([
+            'id' => 1,
+            'transaction_date' => new DateTime("2021-02-13"),
+            'amount' => 4300,
+            'status' => 'OPEN',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+
+        $journalEntries = [
+            [ 'id' => 1, 'amount' => 2500, 'type' => 'CR', 'account_id' => 1, 'transaction_id' => 1 ],
+            [ 'id' => 2, 'amount' => 1800, 'type' => 'CR', 'account_id' => 3, 'transaction_id' => 1 ],
+            [ 'id' => 3, 'amount' => 4300, 'type' => 'DR', 'account_id' => 5, 'transaction_id' => 1 ]
+        ];
+
+        foreach ($journalEntries as $entry) {
+            DB::table('journal_entries')->insert(array_merge([
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ], $entry));
+        }
+
+    }
+}
