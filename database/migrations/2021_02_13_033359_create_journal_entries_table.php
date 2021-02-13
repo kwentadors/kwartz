c2a169b27ae2f9eb19a640aca375ec90e5308613
pool <loop.edward@gmail.com>
2021-02-13 15:02:49 +0800

    Create mdoels JournalEntry and Transaction

---
+++ database/migrations/2021_02_13_033359_create_journal_entries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJournalEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journal_entries', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount', 10, 2);
+            $table->string('type');
+            $table->foreignId('account_id')->constrained('financial_accounts');
+            $table->foreignId('transaction_id')->constrained('transactions');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journal_entries');
+    }
+}
