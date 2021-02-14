072756dcaf4b1c0b80d57276436dcf148afb2b1f
pool <loop.edward@gmail.com>
2021-02-14 10:02:15 +0800

    Add link to resource

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Route;
 
 class TransactionResource extends JsonResource
 {
@@ -19,7 +20,18 @@ class TransactionResource extends JsonResource
             'transaction_date'  => $this->transaction_date,
             'amount'            => $this->amount,
             'description'       => $this->null,
-            'status'            => $this->status
+            'status'            => $this->status,
+            '_links'            => $this->getLinks()
+        ];
+    }
+
+    private function getLinks() {
+        return [
+            [
+                'rel'       =>'self',
+                'href'      => route('transactions.show', ['transaction' => $this->id], false),
+                'method'    => 'GET'
+            ],
         ];
     }
 }
