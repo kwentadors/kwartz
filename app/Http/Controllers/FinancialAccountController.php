fcfebd0cdd4916a47b8736d1f7253b5e0e1a98f8
pool <loop.edward@gmail.com>
2021-02-21 10:23:11 +0800

    Expose endpoint to retrieve all financial accounts

--- app/Http/Controllers/FinancialAccountController.php
+++ app/Http/Controllers/FinancialAccountController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 use App\Models\FinancialAccount;
 use App\Http\Resources\FinancialAccountResource;
+use App\Http\Resources\FinancialAccountCollection;
 
 class FinancialAccountController extends Controller
 {
+
+    public function index() {
+        return new FinancialAccountCollection(FinancialAccount::paginate());
+    }
+
     public function show($id)
     {
         return new FinancialAccountResource(FinancialAccount::findOrFail($id));
