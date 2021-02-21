4e06f09e383d2b59388645296b5024204dd5685e
pool <loop.edward@gmail.com>
2021-02-21 10:23:24 +0800

    Create test for financial account list

--- tests/Feature/FinancialAccountTest.php
+++ tests/Feature/FinancialAccountTest.php
@@ -10,6 +10,44 @@ use App\Models\FinancialAccount;
 
 class FinancialAccountTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function test_index_empty()
+    {
+        $response = $this->get('/api/v1/accounts');
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'data'  => []
+            ]);
+    }
+
+    public function test_indeX_nonempty()
+    {
+        $accounts = FinancialAccount::factory()
+                        ->count(2)->create();
+
+        $response = $this->get('/api/v1/accounts');
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'data'  => [
+                    [
+                        'id'        => $accounts[0]->id,
+                        'name'      => $accounts[0]->name,
+                        'balance'   => $accounts[0]->balance()
+                    ],
+                    [
+                        'id'        => $accounts[1]->id,
+                        'name'      => $accounts[1]->name,
+                        'balance'   => $accounts[1]->balance()
+                    ]
+                ]
+            ]);
+    }
+
     public function test_show()
     {
         $account = FinancialAccount::factory()->create();
