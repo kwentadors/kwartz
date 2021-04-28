a60679ea0bf8ef05bf6d525800de9ab5e47e081c
pool <loop.edward@gmail.com>
2021-04-28 20:22:43 +0800

    Add default financial accounts

--- database/seeders/FinancialAccountSeeder.php
+++ database/seeders/FinancialAccountSeeder.php
@@ -16,13 +16,7 @@ class FinancialAccountSeeder extends Seeder
      */
     public function run()
     {
-        $accounts = [
-            [ 'name' => 'Cash', 'entry_type' => EntryType::DEBIT ],
-            [ 'name' => 'Accounts Receivable', 'entry_type' => EntryType::DEBIT ],
-            [ 'name' => 'Accounts Payable', 'entry_type' => EntryType::CREDIT ],
-            [ 'name' => 'Owner\'s Capital', 'entry_type' => EntryType::CREDIT ],
-            [ 'name' => 'Expense - Utility', 'entry_type' => EntryType::CREDIT ],
-        ];
+        $accounts = $this->getDebitAccounts() + $this->getCreditAccounts();
 
         foreach ($accounts as $account) {
             DB::table('financial_accounts')->insert([
@@ -30,6 +24,66 @@ class FinancialAccountSeeder extends Seeder
                 'entry_type' => $account['entry_type']
             ]);
         }
+    }
 
+    private function getDebitAccounts() {
+        return array_map(function($accountName) {
+            return [
+                'name'  => $accountName,
+                'entry_type' => EntryType::DEBIT
+            ];
+        }, [
+            'Savings - BDO',
+            'Savings - Unionbank',
+            'Savings - Metrobank',
+            'Savings - CIMB',
+            'Travel Fund - Unionbank',
+            'Savings - BPI',
+            'Checkings - Unionbank',
+            'Stock Market - COL',
+            'Stock Market - First Metro Sec',
+            'P2P - SeedIn',
+            'MP2 Investment',
+
+            'Accounts Receivable',
+
+            'Miscellaneous Assets',
+
+            'Condo - Mivela T2U903',
+
+            'Rent Fund - Unionbank',
+
+            // EXPENSE
+            'Expense - Rent',
+            'Expense - Electricity',
+            'Expense - Water',
+            'Expense - Internet',
+            'Expense - Personal',
+            'Expense - Household',
+            'Expense - Netflix',
+            'Expense - FWD Insurance',
+            'Expense - Bank Charges',
+            'Expense - Learning Materials',
+            'Expense - Miscellaneous',
+        ]);
+    }
+
+    private function getCreditAccounts() {
+        return array_map(function($accountName) {
+            return [
+                'name'  => $accountName,
+                'entry_type' => EntryType::CREDIT
+            ];
+        }, [
+            'Income - Arcanys',
+            'Income - APT',
+            'Income - Stock Market',
+            'Income - Eve P2P',
+            'Incentives - Arcanys',
+            'Incentives - Bank',
+            'Incentives - Miscellaneous',
+
+            'Owner\'s Equity'
+        ]);
     }
 }
